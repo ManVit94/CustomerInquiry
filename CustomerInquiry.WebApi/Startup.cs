2b9f7e3fd87f5cf886e1a5335ee5ac77c21c9fed
vitaliy.mandzyak <vitaliy.mandzyak@eleks.com>
2019-04-01 21:49:43 +0300

    Added swagger

--- CustomerInquiry.WebApi/Startup.cs
+++ CustomerInquiry.WebApi/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace CustomerInquiry.WebApi
 {
@@ -27,6 +28,11 @@ namespace CustomerInquiry.WebApi
             services.AddDbContext<CustomerInquiryDbContext>(opt => opt.UseSqlServer(connString));
 
             services.AddTransient<ICustomerService, CustomerService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "CustomerInquiryApi", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -35,6 +41,14 @@ namespace CustomerInquiry.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseMvc();
         }
