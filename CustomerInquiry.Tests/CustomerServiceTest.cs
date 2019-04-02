fb2a872f35fb142d6018ae0b461e427f2857281b
vitaliy.mandzyak <vitaliy.mandzyak@eleks.com>
2019-04-02 14:22:28 +0300

    Added Unit tests for ICustomerService

---
+++ CustomerInquiry.Tests/CustomerServiceTest.cs
@@ -0,0 +1,79 @@
+﻿using CustomerInquiry.DataAccess;
+using CustomerInquiry.DataAccess.DomainModel;
+using CustomerInquiry.DataAccess.Enum;
+using CustomerInquiry.Services;
+using CustomerInquiry.Services.Implementation;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerInquiry.Tests
+{
+    public class CustomerServiceTest : IDisposable
+    {
+        private CustomerInquiryDbContext _context;
+        private ICustomerService _customerService;
+
+        public CustomerServiceTest()
+        {
+            var options = new DbContextOptionsBuilder<CustomerInquiryDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+
+            _context = new CustomerInquiryDbContext(options);
+            _context.Database.EnsureCreated();
+            Seed(_context);
+
+            _customerService = new CustomerService(_context);
+        }
+
+        [Test]
+        public void ShouldReturnCustomerWithTransactions()
+        {
+            var result = _customerService.GetCustomerInfoAsync(1, 5).Result;
+
+            Assert.Positive(result.Data.Transactions.Count);
+        }
+
+        [Test]
+        public void ShouldReturnNotFound()
+        {
+            var result = _customerService.GetCustomerInfoAsync(2, 5).Result;
+
+            Assert.AreEqual(result.Status, ServiceResultStatus.NotFound);
+        }
+
+
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+
+        private void Seed(CustomerInquiryDbContext context)
+        {
+            context.Customers.Add(new Customer
+            {
+                CustomerID = 1,
+                ContactEmail = "cut1@gmail.com",
+                CustomerName = "Customer 1",
+                MobileNo = 123
+            });
+
+            context.Transactions.Add(new Transaction
+            {
+                Amount = 100,
+                CurrencyCode = "USD",
+                CustomerID = 1,
+                Status = TransactionStatus.Canceled,
+                TransactionID = 1,
+                TransactionTime = DateTime.UtcNow
+            });
+
+            context.SaveChanges();
+        }
+
+    }
+}
